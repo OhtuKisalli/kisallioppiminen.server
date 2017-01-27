ad0eb01be523274c6cffcbf6fc910d03e17c71ef
Greatman Lim <greatman.lim@mahti.org>
2017-01-27 18:04:36 +0200

    Revert "Create user with omniauth"
    This reverts commit 564ebcb0d8a0bd3b45b6736a66bdb3ba87ef8a88.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,6 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :trackable, :validatable,
     :omniauthable, :omniauth_providers => [:google_oauth2]
 
-  def self.from_omniauth(auth)
-
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
-
   has_many :checkmarks, dependent: :destroy
   has_many :exercises, through: :checkmarks
 
