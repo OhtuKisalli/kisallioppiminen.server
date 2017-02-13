467c1882602f3dbccf70ebd5179e236c5e791e91
uedi <uedi@users.noreply.github.com>
2017-02-13 20:22:02 +0200

    scoreboard tests

--- spec/controllers/course_spec.rb
+++ spec/controllers/course_spec.rb
@@ -89,12 +89,76 @@ RSpec.describe CoursesController, type: :controller do
         expect(response.status).to eq(200)
         expect(Course.find_by(coursekey:"avain1").exercises.count).to eq(3)
       end
-      
     end
-  
-  
   end
-
+  
+  describe "get scoreboard(s)" do
+    context "when not logged in " do
+      it "gives error message for scoreboard" do
+        get 'scoreboard', :format => :json, params: {"id":"1"}
+        expect(response.status).to eq(401)
+      end
+      it "gives error message for scoreboards" do
+        get 'scoreboards', :format => :json
+        expect(response.status).to eq(401)
+      end
+    end
+    
+    context "when logged in" do
+      before(:each) do
+        @course1 = FactoryGirl.create(:course, coursekey:"key1")
+        @course2 = FactoryGirl.create(:course, coursekey:"key2")
+        @exercise1 = Exercise.create(course_id: @course1.id, html_id:"id1")  
+        @exercise2 = Exercise.create(course_id: @course1.id, html_id:"id2")
+        @opiskelija1 = FactoryGirl.create(:user, username:"o1", name:"pekka", email:"o1@o.o")
+        @opiskelija2 = FactoryGirl.create(:user, username:"o2", name:"bruce", email:"o2@o.o")
+        Attendance.create(user_id: @opiskelija1.id, course_id: @course1.id)
+        Attendance.create(user_id: @opiskelija2.id, course_id: @course1.id)
+        @checkmark1 = Checkmark.create(user_id: @opiskelija1.id, exercise_id: @exercise1.id, status:"green")
+        @checkmark2 = Checkmark.create(user_id: @opiskelija1.id, exercise_id: @exercise2.id, status:"red")
+        @checkmark3 = Checkmark.create(user_id: @opiskelija2.id, exercise_id: @exercise1.id, status:"red")
+        @checkmark4 = Checkmark.create(user_id: @opiskelija2.id, exercise_id: @exercise2.id, status:"green")
+        @ope1 = FactoryGirl.create(:user, username:"ope1", email:"ope1@o.o")
+        Teaching.create(user_id: @ope1.id, course_id: @course1.id)
+        Teaching.create(user_id: @ope1.id, course_id: @course2.id)
+      end
+    
+      it "have to be teacher" do
+        sign_in @opiskelija1
+        get 'scoreboard', :format => :json, params: {"id":@course1.id}
+        expect(response.status).to eq(401)
+        get 'scoreboards', :format => :json
+        expect(response.status).to eq(401)
+      end
+      
+      it "have to be teacher of the course" do
+        @course3 = FactoryGirl.create(:course, coursekey:"key3")
+        @ope2 = FactoryGirl.create(:user, username:"ope2", email:"ope2@o.o")
+        Teaching.create(user_id: @ope2.id, course_id: @course3.id)
+        sign_in @ope2
+        get 'scoreboard', :format => :json, params: {"id":@course1.id}
+        expect(response.status).to eq(204)
+      end
+      
+      it "returns all scoreboards for teacher" do
+        sign_in @ope1
+        get 'scoreboards', :format => :json
+        expect(response.status).to eq(200)
+        body = JSON.parse(response.body)
+        expect(body.keys).to contain_exactly("key1","key2")
+      end
+      
+      it "return a scoreboard for teacher" do
+        sign_in @ope1
+        get 'scoreboard', :format => :json, params: {"id":@course1.id}
+        expect(response.status).to eq(200)
+        body = JSON.parse(response.body)
+        expect(body.keys).to contain_exactly("pekka","bruce")
+      end
+    end
+  end
+  
+    
   describe "Create" do
     it "creates new Course with valid params" do
       course = Course.create! valid_attributes
