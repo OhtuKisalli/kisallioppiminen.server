7083bb1742aaf16b0ac6260f99c6cd241b46a567
uedi <uedi@users.noreply.github.com>
2017-02-22 23:15:34 +0200

    multidatabase

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170221115213) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "attendances", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "breweries", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -45,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170221115213) do
     t.datetime "updated_at", null: false
     t.date     "startdate"
     t.date     "enddate"
-    t.index ["coursekey"], name: "index_courses_on_coursekey", unique: true, using: :btree
+    t.index ["coursekey"], name: "index_courses_on_coursekey", unique: true
   end
 
   create_table "deadlines", force: :cascade do |t|
@@ -103,8 +93,8 @@ ActiveRecord::Schema.define(version: 20170221115213) do
     t.string   "last_sign_in_ip"
     t.string   "first_name"
     t.string   "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
