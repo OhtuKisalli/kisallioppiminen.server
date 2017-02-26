e32940650a5364f799e65f2d06312ef8c7749731
uedi <uedi@users.noreply.github.com>
2017-02-26 14:35:10 +0200

    arkistointitoiminnon testit

---
+++ spec/controllers/teachings_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe TeachingsController, type: :controller do
+
+  describe "teacher toggling archived of courses" do
+  
+    context "when not logged in" do
+      it "gives error message" do
+        post 'toggle_archived', :format => :json, params: {"sid":1,"cid":1,"archived": "true"}
+        expect(response.status).to eq(401)
+        body = JSON.parse(response.body)
+        expected = {"error" => "Sinun täytyy ensin kirjautua sisään."}
+        expect(body).to eq(expected)
+      end
+    end
+    
+    context "when logged in" do
+      before(:each) do
+        @course1 = FactoryGirl.create(:course)
+        @testaaja = FactoryGirl.create(:user)
+        sign_in @testaaja
+      end
+      it "can only toggle archived of own courses" do
+        post 'toggle_archived', :format => :json, params: {"sid":2,"cid":1,"archived": "true"}
+        expect(response.status).to eq(401)
+        body = JSON.parse(response.body)
+        expected = {"error" => "Voit muuttaa vain omien kurssiesi asetuksia."}
+        expect(body).to eq(expected)
+      end
+      it "cannot toggle archived if not on course" do
+        post 'toggle_archived', :format => :json, params: {"sid":1,"cid":1,"archived": "true"}
+        expect(response.status).to eq(403)
+      end
+      it "cannot toggle archived without parameter archived" do
+        Teaching.create(user_id: @testaaja.id, course_id: @course1.id)
+        post 'toggle_archived', :format => :json, params: {"sid":1,"cid":1,"aaa": "true"}
+        expect(response.status).to eq(422)
+      end
+      it "cannot toggle archived with improper value" do
+        Teaching.create(user_id: @testaaja.id, course_id: @course1.id)
+        post 'toggle_archived', :format => :json, params: {"sid":1,"cid":1,"archived": "joo"}
+        expect(response.status).to eq(422)
+      end
+      it "can toggle archived from false to true" do
+        Teaching.create(user_id: @testaaja.id, course_id: @course1.id, archived: false)
+        post 'toggle_archived', :format => :json, params: {"sid":1,"cid":1,"archived": "true"}
+        expect(response.status).to eq(200)
+        body = JSON.parse(response.body)
+        expected = {"message" => "Kurssi arkistoitu."}
+        expect(body).to eq(expected)
+        expect(Teaching.first.archived).to eq(true)
+      end
+      it "can toggle archived from true to false" do
+        Teaching.create(user_id: @testaaja.id, course_id: @course1.id, archived: true)
+        post 'toggle_archived', :format => :json, params: {"sid":1,"cid":1,"archived": "false"}
+        expect(response.status).to eq(200)
+        body = JSON.parse(response.body)
+        expected = {"message" => "Kurssi palautettu arkistosta."}
+        expect(body).to eq(expected)
+        expect(Teaching.first.archived).to eq(false)
+      end
+    end
+  end
+
+end
+
