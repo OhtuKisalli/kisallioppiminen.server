434777a9ea04f729942934d41893cd38f6a74981
uedi <uedi@users.noreply.github.com>
2017-03-05 16:20:51 +0200

    services to simplecov config

--- lib/simplecov_custom_adapter.rb
+++ lib/simplecov_custom_adapter.rb
@@ -12,6 +12,6 @@ SimpleCov.adapters.define 'kisalli' do
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
   add_group 'Helpers', 'app/helpers'
-  add_group 'Libraries', 'lib'
+  add_group 'Services', 'app/services'
   
 end
