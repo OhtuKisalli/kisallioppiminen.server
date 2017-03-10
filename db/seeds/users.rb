ea7bbb6c8645c194abb37e49db09c5111adaef03
uedi <uedi@users.noreply.github.com>
2017-03-10 13:22:08 +0200

    seeds updated

---
+++ db/seeds/users.rb
@@ -0,0 +1,48 @@
+#teachers
+User.create(first_name: "John", last_name: "Kimble", email:"o1@e.e", password:"qwerty") #1
+User.create(first_name: "Mr.", last_name: "Garrison", email:"o2@e.e", password:"qwerty")
+User.create(first_name: "William", last_name: "Forrester", email:"o3@e.e", password:"qwerty")
+User.create(first_name: "Ms.", last_name: "Krabbapel", email:"o4@e.e", password:"qwerty") #4
+
+#students
+User.create(first_name: "James", last_name: "Bond", email:"u1@e.e", password:"qwerty") #5
+User.create(first_name: "Han", last_name: "Solo", email:"u2@e.e", password:"qwerty")
+User.create(first_name: "Indiana", last_name: "Jones", email:"u3@e.e", password:"qwerty")
+User.create(first_name: "Bruce", last_name: "Wayne", email:"u4@e.e", password:"qwerty")
+User.create(first_name: "Ellen", last_name: "Ripley", email:"u5@e.e", password:"qwerty")
+User.create(first_name: "John", last_name: "McClane", email:"u6@e.e", password:"qwerty")
+User.create(first_name: "Tyler", last_name: "Durden", email:"u7@e.e", password:"qwerty")
+User.create(first_name: "Darth", last_name: "Vader", email:"u8@e.e", password:"qwerty")
+User.create(first_name: "Michael", last_name: "Corleone", email:"u9@e.e", password:"qwerty")
+User.create(first_name: "Marty", last_name: "McFly", email:"u10@e.e", password:"qwerty")
+User.create(first_name: "Iron", last_name: "Man", email:"u11@e.e", password:"qwerty")
+User.create(first_name: "Jack", last_name: "Sparrow", email:"u12@e.e", password:"qwerty")
+User.create(first_name: "Ron", last_name: "Burgundy", email:"u13@e.e", password:"qwerty")
+User.create(first_name: "Rick", last_name: "Blaine", email:"u14@e.e", password:"qwerty")
+User.create(first_name: "Doc", last_name: "Brown", email:"u15@e.e", password:"qwerty")
+User.create(first_name: "Hannibal", last_name: "Lecter", email:"u16@e.e", password:"qwerty")
+User.create(first_name: "Travis", last_name: "Bickle", email:"u17@e.e", password:"qwerty")
+User.create(first_name: "Daniel", last_name: "Plainview", email:"u18@e.e", password:"qwerty")
+User.create(first_name: "Patrick", last_name: "Bateman", email:"u19@e.e", password:"qwerty")
+User.create(first_name: "Forrest", last_name: "Gump", email:"u20@e.e", password:"qwerty")
+User.create(first_name: "Snake", last_name: "Plissken", email:"u21@e.e", password:"qwerty")
+User.create(first_name: "Peter", last_name: "Venkman", email:"u22@e.e", password:"qwerty")
+User.create(first_name: "Jules", last_name: "Winnfield", email:"u23@e.e", password:"qwerty")
+User.create(first_name: "Mal", last_name: "Reynolds", email:"u24@e.e", password:"qwerty")
+User.create(first_name: "Atticus", last_name: "Finch", email:"u25@e.e", password:"qwerty")
+User.create(first_name: "Rocky", last_name: "Balboa", email:"u26@e.e", password:"qwerty")
+User.create(first_name: "Walter", last_name: "Sobchak", email:"u27@e.e", password:"qwerty")
+User.create(first_name: "Ferris", last_name: "Bueller", email:"u28@e.e", password:"qwerty")
+User.create(first_name: "Keyser", last_name: "Söze", email:"u29@e.e", password:"qwerty")
+User.create(first_name: "Lou", last_name: "Bloom", email:"u30@e.e", password:"qwerty")
+User.create(first_name: "Anton", last_name: "Chigurh", email:"u31@e.e", password:"qwerty")
+User.create(first_name: "Tommy", last_name: "DeVito", email:"u32@e.e", password:"qwerty")
+User.create(first_name: "Captain", last_name: "America", email:"u33@e.e", password:"qwerty")
+User.create(first_name: "Rick", last_name: "Deckard", email:"u34@e.e", password:"qwerty")
+User.create(first_name: "Lester", last_name: "Burnham", email:"u35@e.e", password:"qwerty")
+User.create(first_name: "Harry", last_name: "Callahan", email:"u36@e.e", password:"qwerty")
+User.create(first_name: "Luke", last_name: "Skywalker", email:"u37@e.e", password:"qwerty")
+User.create(first_name: "Obi-Wan", last_name: "Kenobi", email:"u38@e.e", password:"qwerty")
+User.create(first_name: "Shaun", last_name: "Riley", email:"u39@e.e", password:"qwerty")
+User.create(first_name: "Vito", last_name: "Corleone", email:"u40@e.e", password:"qwerty") #44
+
