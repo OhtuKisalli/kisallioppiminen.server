63cb3de708080f4eb5f9634aee7fd7583fd03656
uedi <uedi@users.noreply.github.com>
2017-03-15 19:42:52 +0200

    attendances refactoring

--- app/services/attendance_service.rb
+++ app/services/attendance_service.rb
@@ -1,20 +1,24 @@
 class AttendanceService
 
+  # returns true or false
   def self.user_on_course?(sid, cid)
     return Attendance.where(user_id: sid, course_id: cid).any?
   end
   
+  # returns true or false
   def self.student_course_archived?(sid, cid)
-    return Attendance.where(user_id: sid, course_id: cid).first.archived
+    return is_archived?(sid,cid)
   end
   
+  # returns []
   def self.all_attendances
     return Attendance.select(:user_id, :course_id, :archived).order(:user_id)
   end
   
+  # returns {}, keys: coursekeys, values: {} with keys "id","coursename","coursekey","html_id","startdate","enddate"
   def self.add_new_course_to_user(sid, cid)
     Attendance.create(user_id: sid, course_id: cid)
-    courses = User.find(sid).courses
+    courses = UserService.student_courses(sid)
     result = {}
     courses.each do |c|
       result[c.coursekey] = CourseService.basic_course_info(c)
@@ -22,20 +26,30 @@ class AttendanceService
     return result
   end
   
+  # returns true or false
   def self.is_archived?(sid, cid)
     return Attendance.where(user_id: sid, course_id: cid).first.archived
   end
   
+  # returns true or false
   def self.create_attendance(sid, cid)
-    Attendance.create(user_id: sid, course_id: cid)
+    @user = UserService.user_by_id(sid)
+    @course = CourseService.course_by_id(cid)
+    if @user and @course and not user_on_course?(sid, cid)
+      Attendance.create(user_id: sid, course_id: cid)
+      return true
+    else
+      return false
+    end
   end
   
+  # returns nothing
   def self.change_archived_status(sid, cid, status)
     a = Attendance.where(user_id: sid, course_id: cid).first
-    if status == "false"
+    if a and status == "false"
       a.archived = false
       a.save
-    elsif status == "true"
+    elsif a and status == "true"
       a.archived = true
       a.save
     end
