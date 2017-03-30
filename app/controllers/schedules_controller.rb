3434f88b6459f100fbeae45dccb0a08bce176b09
uedi <uedi@users.noreply.github.com>
2017-03-30 16:07:05 +0300

    schedules_controller runko

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,5 +7,23 @@ class SchedulesController < ApplicationController
     @schedules = ScheduleService.all_schedules
   end
 
+  # Teacher - add schedule to schedules list
+  # post '/courses/:id/schedules/new' 
+  def new_schedule
+  
+  end
+  
+  # Teacher - delete schedule
+  # delete '/courses/:cid/schedules/:did' 
+  def delete_deadline
+  
+  end
+  
+  # Teacher - Add / update exercises of schedules created
+  # post '/courses/:id/schedules/'
+  def update_exercises
+  
+  end
+
 
 end
