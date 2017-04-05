93a5ce037b13c84815f971481033d2c193502b16
uedi <uedi@users.noreply.github.com>
2017-04-05 13:33:38 +0300

    student/teacher tieto sessiion

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -22,9 +22,21 @@ RSpec.describe UsersController, type: :controller do
       sign_in @user
       get 'get_session_user', :format => :json
       body = JSON.parse(response.body)
-      expected = {"has_sign_in" => {"id" => @user.id, "first_name" => @user.first_name}}
+      expected = {"has_sign_in" => {"id" => @user.id, "first_name" => @user.first_name, "teacher"=>false, "student"=>false}}
       expect(body).to eq(expected)
     end
+    it "get_session_user tells if user is teacher and student" do
+      @student = FactoryGirl.create(:user, first_name:"Bruce", email:"o4@o.o")
+      @course = FactoryGirl.create(:course, coursekey:"key1")
+      Attendance.create(user_id: @student.id, course_id: @course.id)
+      Teaching.create(user_id: @student.id, course_id: @course.id)
+      sign_in @student
+      get 'get_session_user', :format => :json
+      body = JSON.parse(response.body)
+      expected = {"has_sign_in" => {"id" => @student.id, "first_name" => @student.first_name, "teacher"=>true, "student"=>true}}
+      expect(body).to eq(expected)
+    end
+    
     it "get_session_user returns nil for user not logged in" do
       get 'get_session_user', :format => :json
       body = JSON.parse(response.body)
